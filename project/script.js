1693b74dc53fc11f8ee8275749a6633b7976bb27
LusterPearl <Salamioluwatosin90@gmail.com>
2025-01-15 16:05:33 +0100

    all

---
+++ project/script.js
@@ -0,0 +1,40 @@
+console.lo("=== Scope ===")
+let message = "Hello, World";
+const author = "Tosin";
+
+console.log(message, author);
+
+//Arrow function
+console.log("=== Arrow Functions ===");
+const square = x => x * x;
+console.log(`Square of 5: ${square(5)}`);
+
+// Array methods
+console.log("=== Array Methods ===");
+const numbers = [1, 2, 3, 4, 5];
+
+//map()
+const squares = numbers.map(num => num * num);
+console.log("Squares:", squares);
+
+// filter()
+const evens = numbers.filter(num => num % 2 === 0);
+console.log("Even Numbers:", evens);
+
+// reduce()
+const sum = numbers.reduce((total, num) => total + num, 0);
+console.log("Sum of Numbers:", sum);
+
+// forEach()
+numbers.forEach(num => console.log("Number:", num));
+
+// objects
+console.log("===Objects ===");
+const person = {
+    name: "Tosin",
+    age: 25,
+    greet() {
+        return `Hi, I'm ${this.name}, and I'M ${this.age} years old.`;
+    },
+};
+console.log(person.greet());
